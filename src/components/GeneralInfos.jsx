f2bce2716000e362e8eaefeb2714e8f70a98ca4a
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-03-24 15:11:13 +0100

    style: change icons size

--- src/components/GeneralInfos.jsx
+++ src/components/GeneralInfos.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import WeatherIcon from "../assets/weather-icons/Clear_Day.svg";
+import WeatherIcon from "../assets/weather-icons/Few-Clouds_Night.svg";
 
 const GeneralInfos = () => {
   return (
     <div className="general-infos flex flex-col justify-between w-full p-4 text-lg">
-      <div className="time-and-location mb-10">
+      <div className="time-and-location mb-10 ms-10">
         <h1>Istanbul, TR</h1>
-        <p>Sunday, May 24, 2024</p>
+        <p className="text-md">Sunday, May 24, 2024</p>
       </div>
       <div className="general-weather flex flex-row justify-between">
-        <div className="w-1/2 flex flex-col justify-end">
-          <h1>28°C</h1>
+        <div className="w-1/2 flex flex-col justify-center ms-10">
+          <h1 className="text-xl">28°C</h1>
           <p>26°C / 32°C</p>
-          <p>Clear</p>
+          <p className="text-md">Few Clouds</p>
         </div>
         <div className="w-1/2">
           <img src={WeatherIcon} alt="" className="ms-auto" />
