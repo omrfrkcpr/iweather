2f18873c27b78ccb232b1fae30fc42a869549971
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-03-30 04:27:09 +0100

    style: responsive pages + add Logo ShowWeather page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -6,8 +6,8 @@ import { ToastContainer } from "react-toastify";
 
 const Main = () => {
   return (
-    <div className="bg-main-container py-5 flex justify-center items-center h-screen">
-      <div className="absolute top-0 left-0 w-full">
+    <div className="p-4 flex justify-center items-center h-screen">
+      <div className="absolute p-4 top-0 left-0 w-full">
         <Logo />
       </div>
       <div className="flex flex-col items-center w-full">
--- src/pages/ShowWeather.jsx
+++ src/pages/ShowWeather.jsx
@@ -5,22 +5,26 @@ import Forecast from "../components/Forecast";
 import { WeatherContext } from "../context/WeatherProvider";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Logo from "../components/Logo";
 
 const ShowWeather = () => {
   const { weather } = useContext(WeatherContext);
   return (
-    <>
-      {weather && (
-        <div className="gap-1 p-3 flex justify-start align-middle text-white flex-col md:flex-row">
-          <GeneralInfos />
-          <div>
-            <WeatherDetails />
-            <Forecast />
+    <div className="absolute inset-0 flex items-top justify-center p-4 max-[600]">
+      <div>
+        <Logo />
+        {weather && (
+          <div className="gap-1 p-3 flex justify-start align-middle text-white flex-col md:flex-row  ">
+            <GeneralInfos />
+            <div className="p-1">
+              <WeatherDetails />
+              <Forecast />
+            </div>
           </div>
-        </div>
-      )}
-      <ToastContainer />
-    </>
+        )}
+        <ToastContainer />
+      </div>
+    </div>
   );
 };
 
