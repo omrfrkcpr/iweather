a428c76db46d53e4c575318858f86f10d5326bac
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-04-01 21:21:56 +0200

    chore: Footer -  add copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,13 @@
-import React, { useContext } from "react";
+import React from "react";
 import { FaGithubSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className=" text-product absolute center bottom-5 flex gap-2 w-full">
+      <h1 className="text-responsive">Copyright &copy; Omer Faruk Capur </h1>
       <a href="https://github.com/omrfrkcpr" target="blank">
-        <FaGithubSquare
-          size={28}
-          className="hover:text-black hover:bg-white"
-        />
+        <FaGithubSquare size={28} className="hover:text-black hover:bg-white" />
       </a>
       <a href="https://www.linkedin.com/in/omrfrkcpr/" target="blank">
         <FaLinkedin
