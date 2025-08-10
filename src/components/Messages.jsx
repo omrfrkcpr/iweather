c9945451e75a5e11ae46a381985d9edc5642609f
omrfrkcpr <omerrfarukcapur@gmail.com>
2025-08-10 13:41:33 +0200

    update product title

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -4,7 +4,7 @@ const Messages = () => {
   return (
     <div className="text-center my-6">
       <h1 className="text-base-100 header-responsive ">
-        Welcome to <span className="text-product">TypeWeather</span>
+        Welcome to <span className="text-product">IWeather</span>
       </h1>
       <p className="text-base-200 text-responsive">
         Choose a location to see the weather forecast
